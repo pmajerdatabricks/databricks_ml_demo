bb105e244dee88b487c73d07e23c8a156b6a3053
mshtelma <mshtelma>
2021-08-20 19:11:44 +0000

    Added automatic repo creation

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,4 +1,10 @@
 # Databricks notebook source
+#import os
+#os.environ['DATABRICKS_HOST'] = 'https://eastus2.azuredatabricks.net/'
+#os.environ['DATABRICKS_TOKEN'] = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
+
+# COMMAND ----------
+
 new_cluster_config = """
 {
     "spark_version": "7.3.x-scala2.12",
@@ -10,9 +16,27 @@ new_cluster_config = """
 }
 """
 # Existing cluster ID where integration test will be executed
-existing_cluster_id = '0923-164208-meows279'
+existing_cluster_id = '0820-100141-forum325'
 # Path to the notebook with the integration test
-notebook_path = '/Repos/michael.shtelma@databricks.com/ml_demo_msh/test/unittest_model'
+notebook_path = '/test/unittest_model'
+repo_path = '/Repos/michael.shtelma@databricks.com/databricks_ml_demo'
+
+
+repos_path_prefix='/Repos/michael.shtelma@databricks.com/databricks_ml_demo'
+git_url = 'https://github.com/mshtelma/databricks_ml_demo'
+provider = 'gitHub'
+branch = 'main'
+
+# COMMAND ----------
+
+from argparse import ArgumentParser
+p = ArgumentParser()
+p.add_argument("--branch", required=False, type=str)
+namespace = p.parse_known_args()[0]
+conf_branch = namespace.branch
+if conf_branch is not None:
+  branch = conf_branch
+
 
 # COMMAND ----------
 
@@ -27,14 +51,22 @@ from databricks_cli.sdk import JobsService
 config = EnvironmentVariableConfigProvider().get_config()
 api_client = _get_api_client(config, command_name="cicdtemplates-")
 
-# Let's update our Repo to the latest git revision using Repos API
-res = api_client.perform_query('PATCH','/repos/{repo_id}'.format(repo_id="3814111933528092"), {"branch":"master"})
-print(res)
+#Let's create Repos Service
+repos_service = ReposService(api_client)
+
+# Let's store the path for our new Repo
+repo_path = f'{repos_path_prefix}_{branch}_{str(datetime.now().microsecond)}'
+
+# Let's clone our GitHub Repo in Databricks using Repos API
+repo = repos_service.create_repo(url=git_url, provider=provider, path=repo_path)
+
+#Let's checkout the needed branch
+repos_service.update_repo(id=repo['id'], branch=branch)
 
 #Let's create a jobs service to be able to start/stop Databricks jobs
 jobs_service = JobsService(api_client)
 
-notebook_task = {'notebook_path': notebook_path}
+notebook_task = {'notebook_path': repo_path + notebook_path}
 #new_cluster = json.loads(new_cluster_config)
 
 # Submit integration test job to Databricks REST API
